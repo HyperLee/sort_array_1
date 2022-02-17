b40d704cd41369e96f33919b73718935c06a202d
Peter.Chiu <tidub76217@hotmail.com>
2022-02-18 00:01:22 +0800

    Initial Commit

---
+++ sort_array_1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sort_array_1
+//
+//  Created by HyperLee on 2022/2/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
